2090d45c35161f9cb8193edad7672e05a7dee910
melnik <melniy4uk@gmail.com>
2026-04-13 02:04:44 +0000

    refactor(nvim-tree): вынести настройки в configs

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,10 +1,8 @@
 return {
   {
     "nvim-tree/nvim-tree.lua",
-    opts = function(_, opts)
-      opts.view = opts.view or {}
-      opts.view.width = 25
-      return opts
+    config = function()
+      require("configs.nvim-tree")
     end,
   },
 }
